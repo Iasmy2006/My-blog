a7fc81adde1140a80389c5cfcabe1fe5fe33a9bd
Iasmy2006 <159617189+Iasmy2006@users.noreply.github.com>
2024-02-10 11:48:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href= 'index.html';
+    }
+    
+    function openReviews() {
+    window.location.href= 'reviews.html';
+    }
+    
+    function openContact() {
+    window.location.href= 'contact.html';
+    }
